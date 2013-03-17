ec5b00e271812753b15bc9a91fff353dd0df1493
Sara Van Buskirk <svanbusk@unca.edu>
2013-03-17 01:17:53 -0400

    Add tabs from in-class example.

--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -1,5 +1,31 @@
 var main = function () {
   console.log("hello world");
+  $.getJSON("all.json", function (todos) {
+    var i;
+    for (i = 0; i < todos.length; i++) {
+      console.log(todos[i]);
+    };
+    
+    todos.forEach(function (todo) {
+      console.log(todo.description);
+      todo.categories.forEach(function (category) {
+        console.log("  " + category);
+      });
+    });
+  });
+  
+  var setUpClickHandler = function (anchor) {
+    anchor.click(function () {
+      var target = $(this).attr("href");
+    
+      $(".active").removeClass("active");
+      $(this).addClass("active");
+      $("#"+target).addClass("active");
+    
+      return false;
+    });    
+  };
+  setUpClickHandler($(".tabcontainer .tab"));
 };
 
 $(document).ready(main);
