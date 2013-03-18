f2a22d853f53d20751e434352a5ddebb9f844853
Sara Van Buskirk <svanbusk@unca.edu>
2013-03-18 04:03:09 -0400

    Ensure project is JSLint-compliant so far.

--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -1,66 +1,70 @@
-var main = function () {
+/*global window*/
+(function () {
   "use strict";
-  var stack = new Array();
+  var $ = window.$,
+    document = window.document,
+    main = function () {
+      var stack = [],
+        i = 0,
+        setUpClickHandler = function (anchor) {
+          anchor.click(function () {
+            var target = $(this).attr("href");
 
-  $.getJSON("all.json", function (todos) {
-    
-    //populates All tab
-    todos.forEach(function (todo) {
-      $("#tab1").append("<h3>" + todo.description + "</h3>" + "<h4>tagged: </h4>");
-      todo.categories.forEach(function (category) {
-        $("#tab1").append("<p>" + category + "</p>");
+            $(".active").removeClass("active");
+            $(this).addClass("active");
+            $("#" + target).addClass("active");
+
+            return false;
+          });
+        };
+
+      $.getJSON("all.json", function (todos) {
+
+        //populates All tab
+        todos.forEach(function (todo) {
+          $("#tab1").append("<h3>" + todo.description + "</h3>" + "<h4>tagged: </h4>");
+          todo.categories.forEach(function (category) {
+            $("#tab1").append("<p>" + category + "</p>");
+          });
+          $("#tab1").append("<br />");
+        });
+        //end All tab
+
+
+        //populates Categories tab    
+        todos.forEach(function (todo) {
+          todo.categories.forEach(function (category) {
+            stack.push(category);
+          });
+        });
+
+        //http://stackoverflow.com/questions/5381621/jquery-function-to-get-all-unique-elements-from-an-array
+        var unique = stack.filter(function (itm, i, a) {
+          return i === a.indexOf(itm);
+        });
+
+        unique.forEach(function (tag) {
+          $("#tab2").append("<div id =" + unique[i] + "><h3>" + tag + "</h3></div><br />");
+          i += 1;
+        });
+
+        todos.forEach(function (todo) {
+          todo.categories.forEach(function (category) {
+            $("#" + category).append("<p>" + todo.description + "</p>");
+          });
+        });
+        //end Categories tab
+
+
+        //Add tab
+
+        //end Add tab
       });
-      $("#tab1").append("<br />");
-    });
-    //end All tab
-    
-    
-    //populates Categories tab    
-    todos.forEach(function (todo) {
-      todo.categories.forEach(function (category) {        
-        stack.push(category);
-      });
-    });
-    
-    //http://stackoverflow.com/questions/5381621/jquery-function-to-get-all-unique-elements-from-an-array
-    var unique=stack.filter(function (itm, i, a){
-      return i === a.indexOf(itm);
-    });
-    
-    var i = 0;    
-    unique.forEach(function (tag) {
-      $("#tab2").append("<div id =" + unique[i] + "><h3>" + tag + "</h3></div><br />");
-      i += 1;
-    });
-    
-    todos.forEach(function(todo) {
-      todo.categories.forEach(function (category) {
-        $("#" + category).append("<p>" + todo.description + "</p>");
-      });
-    });
-    //end Categories tab
-    
-    
-    //Add tab
-    
-    //end Add tab
-    
-  });
 
-  var setUpClickHandler = function (anchor) {
-    anchor.click(function () {
-      var target = $(this).attr("href");
+      setUpClickHandler($(".tabcontainer .tab"));
 
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-      $("#" + target).addClass("active");
+    };
 
-      return false;
-    });
-  };
-  
-  setUpClickHandler($(".tabcontainer .tab"));
-  
-};
+  $(document).ready(main);
 
-$(document).ready(main);
+}());
