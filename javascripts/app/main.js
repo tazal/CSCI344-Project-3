4a623943d259e06de7ca8d8e55fb28938fd8f19d
Sara Van Buskirk <svanbusk@unca.edu>
2013-03-17 22:47:31 -0400

    Add content from all.json to first tab. Start on second tab.

--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -1,30 +1,46 @@
 var main = function () {
+  "use strict";
   console.log("hello world");
   $.getJSON("all.json", function (todos) {
-    var i;
+    
+    //populates Tab 1
+    todos.forEach(function (todo) {
+      $("#tab1").append("<h3>" + todo.description + "</h3>" + "<h4>tagged: </h4>");
+      todo.categories.forEach(function (category) {
+        $("#tab1").append("<p>" + category + "</p>");
+      });
+    });
+    
+    //populates Tab 2
+    todos.forEach(function (todo) {
+      todo.categories.forEach(function (category) {
+        if (isUnique(category)) {
+          $("#tab2").append("<h3>" + category + "</h3>");
+        }
+      });
+    });  
+  });
+  
+  var isUnique = function (string) {
+    var i,
+      stack = new Array(1);
     for (i = 0; i < todos.length; i++) {
       console.log(todos[i]);
     };
-    
-    todos.forEach(function (todo) {
-      console.log(todo.description);
-      todo.categories.forEach(function (category) {
-        console.log("  " + category);
-      });
-    });
-  });
-  
+  }
+
   var setUpClickHandler = function (anchor) {
     anchor.click(function () {
       var target = $(this).attr("href");
-    
+
       $(".active").removeClass("active");
       $(this).addClass("active");
-      $("#"+target).addClass("active");
-    
+      $("#" + target).addClass("active");
+
       return false;
-    });    
+    });
   };
+  
   setUpClickHandler($(".tabcontainer .tab"));
 };
 
